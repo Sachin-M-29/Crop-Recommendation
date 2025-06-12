b9af0eef3ba7e0279e465dddacecd32bc2c7076d
Sachin M <sachinmanickam2005@gmail.com>
2025-06-12 16:37:20 +0530

    backend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "prefer-const": "off", // disable the error you saw
+    },
+  },
 ];
 
 export default eslintConfig;
